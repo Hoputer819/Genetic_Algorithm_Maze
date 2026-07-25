d5bde3156bb03a4126331680dce76d264205aa9f
Hoputer819 <programmer0819@gmail.com>
2026-07-26 03:33:19 +0900

    유전 알고리즘 프로그램 완성,근데 최악의 경우를 생각해 최대 세대 수를 정하는게 좋을듯

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,20 @@ import genetic.GeneticMover;
 
 public class Main {
     public static void main(String[] args) {
+
+        long startTime = System.currentTimeMillis();
+
         MazeGenerator maze = new MazeGenerator(5);
         maze.generate();
+
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
 
         GeneticMover.maze = maze;
         WindowGenerator.maze = maze;
         Application.launch(WindowGenerator.class);
+
+        System.out.println("실행 시간: " + duration + " ms (" + (duration / 1000.0) + " 초)");
 
     }
 }
--- src/genetic/GeneticMover.java
+++ src/genetic/GeneticMover.java
@@ -11,18 +11,24 @@ public class GeneticMover {
 
     int individual_num;
     int gene_num;
+    int elite_count;
+    int run_x;
+    int run_y;
+    double individual_mutation_rate;
+    double gene_mutation_rate;
 
     //객체 리스트
-    ArrayList<Individual> current_population = new ArrayList<>();
-    ArrayList<Individual> next_population = new ArrayList<>();
+    ArrayList<Individual> population = new ArrayList<>();
 
     //생성자
     public GeneticMover(){
-        individual_num =  (maze.size * maze.size)/2;
+        individual_num =  (maze.size * maze.size) / 2;
         gene_num = (maze.size * maze.size) * 2;
+        individual_mutation_rate = 1.0 / (2 * maze.size);
+        gene_mutation_rate = 1.0 * (maze.size) / gene_num;
+        elite_count = maze.size >= 10  ? (int)(individual_num * 0.1) : 2;
         for(int i = 0; i < individual_num; i++){
-            current_population.add(new Individual(gene_num));
-            next_population.add(new Individual(gene_num));
+            population.add(new Individual(gene_num));
         }
         gene_reset();
     }
@@ -31,40 +37,41 @@ public class GeneticMover {
     private void gene_reset(){
         for(int i =  0; i < individual_num; i++){
             for(int j = 0; j < gene_num; j++)
-                current_population.get(i).gene[j] = random.nextInt(4);
+                population.get(i).gene[j] = random.nextInt(4);
         }
     }// 0 : 오른쪽, 1 : 왼쪽, 2 : 위, 3 : 아래
 
     //선택 메서드
     private void selection(){
-        int visit_again = 0;
+        int visit_again;
         int big_individual;
-        int max_distance = maze.last_map[maze.size-1][0];
+        int max_distance = maze.back_map[maze.size-1][0];
         Individual temp;
 
         //적합도 평가
         for(int i =  0; i < individual_num; i++){
+            visit_again = 0;
 
             //갔던길을 한번 더 가면 -
             for(int j = 0; j < maze.size; j++){
                 for(int k = 0; k < maze.size; k++){
-                    if(current_population.get(i).visited[j][k] > 1)
-                        visit_again += (current_population.get(i).visited[j][k] - 1);
+                    if(population.get(i).visited[j][k] > 1)
+                        visit_again += (population.get(i).visited[j][k] - 1);
                 }
             }
-            current_population.get(i).fitness -= visit_again * 10;
+            population.get(i).fitness -= visit_again * 10;
 
             //목적지와의 거리(BFS 활용) +
-            current_population.get(i).fitness += (max_distance - maze.back_map[current_population.get(i).die_y][current_population.get(i).die_x]) * 20;
+            population.get(i).fitness += (max_distance - maze.back_map[population.get(i).die_y][population.get(i).die_x]) * 20;
 
             //움직인 칸 수 +
-            current_population.get(i).fitness += current_population.get(i).cell_move * 10;
+            population.get(i).fitness += population.get(i).cell_move * 10;
 
             //목적지 도착 +
-            if(current_population.get(i).die_x == maze.last_x && current_population.get(i).die_y == maze.last_y) {
+            if(population.get(i).die_x == maze.last_x && population.get(i).die_y == maze.last_y) {
                 //이동한 유전자 길이 -
-                current_population.get(i).fitness -= (current_population.get(i).cell_move) * 20;
-                current_population.get(i).fitness += 100000;
+                population.get(i).fitness -= (population.get(i).cell_move) * 20;
+                population.get(i).fitness += 100000;
             }
         }
 
@@ -72,56 +79,140 @@ public class GeneticMover {
         for(int i = 0; i < individual_num-1; i++){
             big_individual = i;
             for(int j = i+1; j < individual_num; j++){
-                if(current_population.get(j).fitness > current_population.get(big_individual).fitness)
+                if(population.get(j).fitness > population.get(big_individual).fitness)
                     big_individual = j;
             }
 
-            temp = current_population.get(i);
-            current_population.set(i,current_population.get(big_individual));
-            current_population.set(big_individual,temp);
+            temp = population.get(i);
+            population.set(i,population.get(big_individual));
+            population.set(big_individual,temp);
         }
 
     }
 
-    /*
-    교차 메서드
+    //교차 메서드
+    private void crossover(){
+        int mom;
+        int dad;
 
-        1.상위 개체 유전자 추출
+        for(int i = elite_count; i < individual_num; i++){
+            mom = random.nextInt(elite_count);
+            do {
+                dad = random.nextInt(elite_count);
+            }while(mom == dad);
 
-        2.상위 개체 유전자로 교차
+            for(int j = 0; j < gene_num; j++){
+                if(j < gene_num/2)
+                    population.get(i).gene[j] = population.get(mom).gene[j];
+                else
+                    population.get(i).gene[j] = population.get(dad).gene[j];
+            }
+        }
+    }
 
-     */
+    //변이 메서드
+    private void mutation(){
+        for(int i = elite_count; i < individual_num; i++){
+            if(Math.random() < individual_mutation_rate){
+                for(int j = 0; j < gene_num; j++){
+                    if(Math.random() < gene_mutation_rate)
+                        population.get(i).gene[j] = random.nextInt(4);
+                }
+            }
+        }
+    }
 
-    /*
-    대치 메서드
+    //대치 메서드
+    private void replace(){
 
-        1.범위만큼 next_population에 저장
+        selection();
+        crossover();
+        mutation();
+    }
 
-        2.다시 current_population으로 저장
+    //개체 움직이기 메서드
+    public void run(){
 
-     */
+        do {
+            for(int k = 0; k < individual_num; k++){
+                population.get(k).fitness = 0;
+                population.get(k).cell_move = 0;
+                for(int l = 0; l < maze.size; l++){
+                    for(int m = 0; m < maze.size; m++)
+                        population.get(k).visited[l][m] = 0;
+                }
+            }
 
-    /*
-    변이 메서드
+            for (int i = 0; i < individual_num; i++) {
+                run_x = 0;
+                run_y = maze.size - 1;
 
-        1.객체 선택
+                gene_LOOP:
+                for (int j = 0; j < gene_num; j++) {
+                    switch (population.get(i).gene[j]) {
+                        case 0:
+                            if (maze.map[run_y][run_x].direction[population.get(i).gene[j]]) {
+                                run_x += 1;
+                                population.get(i).visited[run_y][run_x] += 1;
+                                population.get(i).cell_move += 1;
+                                if(run_x == maze.last_x && run_y == maze.last_y)
+                                    break gene_LOOP;
+                            }
+                            else {
+                                break gene_LOOP;
+                            }
+                            break;
+                        case 1:
+                            if (maze.map[run_y][run_x].direction[population.get(i).gene[j]]) {
+                                run_x -= 1;
+                                population.get(i).visited[run_y][run_x] += 1;
+                                population.get(i).cell_move += 1;
+                                if(run_x == maze.last_x && run_y == maze.last_y)
+                                    break gene_LOOP;
+                            }
+                            else {
+                                break gene_LOOP;
+                            }
+                            break;
+                        case 2:
+                            if (maze.map[run_y][run_x].direction[population.get(i).gene[j]]) {
+                                run_y -= 1;
+                                population.get(i).visited[run_y][run_x] += 1;
+                                population.get(i).cell_move += 1;
+                                if(run_x == maze.last_x && run_y == maze.last_y)
+                                    break gene_LOOP;
+                            }
+                            else {
 
-        2.그 객체에 유전자 선택
+                                break gene_LOOP;
+                            }
+                            break;
+                        case 3:
+                            if (maze.map[run_y][run_x].direction[population.get(i).gene[j]]) {
+                                run_y += 1;
+                                population.get(i).visited[run_y][run_x] += 1;
+                                population.get(i).cell_move += 1;
+                                if(run_x == maze.last_x && run_y == maze.last_y)
+                                    break gene_LOOP;
+                            }
+                            else {
+                                break gene_LOOP;
+                            }
+                            break;
+                    }
 
-        3.변이
-     */
 
-    /*
-    객체 움직이기 메서드
+                }
+                population.get(i).die_x = run_x;
+                population.get(i).die_y = run_y;
 
-        1.벽 만날때까지 유전자대로 진행
+            }
 
-        2.상위 10% 대치
+            replace();
 
-        3.상위 10%의 유전자로 1점 교차 및 대치
+        } while (population.getFirst().cell_move != maze.last_map[maze.last_y][maze.last_x] || population.getFirst().die_x != maze.last_x || population.getFirst().die_y != maze.last_y);
+    }
 
-        4.상위 10%를 제외한 나머지 객체들 중 20%의 객체의 유전자에 5%의 변이 적용
-     */
 
 
 }
